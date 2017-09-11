aa6622229e067af90ac2bff68623fa4ba3011baf
Reez Patel <reezpatel@gmail.com>
2017-09-11 16:35:55 +0530

    Added Cooridation Std Password reset

--- login/coordinator/student.password.change.php
+++ login/coordinator/student.password.change.php
@@ -27,24 +27,22 @@ if(!$db)
     die("Failed to Insert: ".mysqli_error($link));
 
 if(mysqli_num_rows($db) > 0) {
-    if(mysqli_query($link,$sql)) {
-        while($row = mysqli_fetch_assoc($db)) {
-            $studentID = $row['USER_ID'];
+    while($row = mysqli_fetch_assoc($db)) {
+        $studentID = $row['USER_ID'];
+        echo "$studentID \n";
+        // Update password in USERS TABLE
+        $sql2 = "UPDATE `$TABLE_NAME` SET `PASSWORD` = '$password' WHERE `USER_ID` = '$studentID'";
+        $db2 = mysqli_query($link,$sql2);
+        if(!$db2)
+            die("Failed to Update Student Password in USERS REQ".mysqli_error($link));
 
-            // Update password in USERS TABLE
-            $sql2 = "UPDATE `$TABLE_NAME` SET `PASSWORD` = '$password' WHERE `USER_ID` = '$studentID'";
-            $db2 = mysqli_query($link,$sql2);
-            if(!$db2)
-                die("Failed to Update Student Password in USERS REQ".mysqli_error($link));
+        // Update password in USERS REQ TABLE
+        $password = md5($password);
+        $sql2 = "UPDATE `$TABLE_NAME_USERS` SET `PASSWORD` = '$password' WHERE `USER_ID` = '$studentID'";
+        $db2 = mysqli_query($link,$sql2);
+        if(!$db2)
+            die("Failed to Update Student Password in USERS RECORD".mysqli_error($link));
 
-            // Update password in USERS REQ TABLE
-            $password = md5($password);
-            $sql2 = "UPDATE `$TABLE_NAME_USERS` SET `PASSWORD` = '$password' WHERE `USER_ID` = '$studentID'";
-            $db2 = mysqli_query($link,$sql2);
-            if(!$db2)
-                die("Failed to Update Student Password in USERS RECORD".mysqli_error($link));
-
-        }
     }
 }
 
